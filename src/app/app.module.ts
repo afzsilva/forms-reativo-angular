5317d5ffd7c1710482ea148bbcd24b866effb396
afzsilva <afzsilva@gmail.com>
2023-03-19 15:26:10 -0300

    Cria formulário component formulario simples

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { FormularioComponent } from './clientes/formulario/formulario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormularioComponent
   ],
   imports: [
     BrowserModule,
